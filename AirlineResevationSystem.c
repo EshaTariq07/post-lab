4e684832d3a9b84462b35493b967d9c0722a725e
FizaSrafraz04 <bitf21m027@pucit.edu.pk>
2022-10-31 20:19:08 +0500

    code for type 1

---
+++ AirlineResevationSystem.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main(void){
+	int array[99]={0};
+	printf("Type 1 for \" first class\"\n");
+	printf("Type 2 for \"Economy class\"\n");
+	int i,a,select;
+	for(i=0;i<99;i++){
+		printf("select type of class: ");
+	scanf_s("%d", &select);
+	    if (select == 1) {
+		printf("please select a seat number(1-30) ");
+		scanf_s("%d", &a);
+		if (a > 30) {
+			printf("please enter apprpriate seat  num\n");
+			scanf_s("%d", &a);
+		}
+		if (array[a] == 0) {
+			printf("seat is available\n");
+			printf("your request is accepted\nyour boarding pass is here\n");
+			
+			printf("======================================\n");
+			printf("|| your seat number is : %d           ||\n",a);
+			printf("|| you're travelling in first class  ||\n");
+			printf("======================================\n");	
+		}
+	}
+
+	   array[a]=1;
+			if (array[a] == 1)
+                           	{
+		printf("seat is booked\n");
+             	}		
+		
+
+
+	if(select==4){
+		int j;
+		for(j=0;j<99;j++){
+			printf("%d",array[j]);
+			
+		}
+		printf("\n");
+	}
+	if (select==3){
+		break;
+	}
+}
+}
+
+
+
+
+
+
+
+
